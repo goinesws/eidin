55f17af75f39c8c882d5c33694d8dffd0d9f6112
Anthony Anthony <anstronott@gmail.com>
2022-07-01 11:10:22 +0700

    test anthony

--- resources/views/frontend/userProfile/donationHistory.blade.php
+++ resources/views/frontend/userProfile/donationHistory.blade.php
@@ -1,7 +1,8 @@
 @extends('partial.headerFooter')
 
 @section('content')
-<div class="container" style="margin-top: 20px;margin-bottom:20px">
-    <h3>my donations</h3>
-</div>
+    <div class="container" style="margin-top: 20px;margin-bottom:20px">
+        {{-- Ngetes bang --}}
+        <h3>my donation</h3>
+    </div>
 @endsection
