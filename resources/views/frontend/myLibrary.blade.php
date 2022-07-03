782d239071cbc23f977a5d65c84928e0e40bb2b6
Yuvenss <levandio_yuvensjonathan@yahoo.co.id>
2022-07-03 22:56:41 +0700

    library

--- resources/views/frontend/myLibrary.blade.php
+++ resources/views/frontend/myLibrary.blade.php
@@ -37,22 +37,5 @@
             </div>
         </div>
     </section>
-
-
-    {{-- <div class="row">
-        @foreach ($myGames as $item)
-            <div class="col-sm-6 col-md-4">
-                <div class="thumbnail mb-5" style="height: 400px; width:400px;">
-                <?php
-                    $promotional = json_decode($item->promotional);
-                    ?>
-                    <img class="card-img-top" src="{{ $promotional->img[0] }}"alt="#">
-                    <div class="caption" style="display: flex; justify-content:center; align-items:center">
-                        <h3><a href="/game/{{ $item->id }}">{{ $item->game_name }}</a></h3>
-                    </div>
-                </div>
-            </div>
-        @endforeach
-    </div> --}}
 </div>
 @endsection
