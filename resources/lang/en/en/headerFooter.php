971d30921b83eb8eea7a19cff13704dd8d7e8137
Yuvenss <levandio_yuvensjonathan@yahoo.co.id>
2022-07-04 12:40:32 +0700

    bahasa inggris

---
+++ resources/lang/en/en/headerFooter.php
@@ -0,0 +1,53 @@
+<?php 
+    return[
+        'header' => [
+            'discover_game' => 'Discover Game',
+            'category' => 'Game Category',
+            'home' => 'Home',
+            'all_games' => 'All Games',
+            'login' => 'Login',
+            'library' => 'Library',
+            'wishlist' => 'Wishlist',
+            'profile' => 'Profile',
+            'my_profile' => 'My Profile',
+            'donation_history' => 'Donation History',
+            'uploaded_games' => 'Uploaded Games',
+            'reviews' => 'Reviews',
+            'purchases&donations' => 'Purchases & Donations',
+            'pending_games' => 'Pending Games',
+            'pending_update' => 'Pending Update',
+        ],
+        'footer' => [
+            'footer_top' => [
+                '1' => 'Subscribe to our newsletter',
+                '2' => 'Get all the latest information, Sales and Offers.',
+                'btn' => 'Subscribe',
+            ],
+            'footer_middle' => [
+                '1' => 'Get In Touch With Us',
+                '2' => 'Phone: +1 (900) 33 169 7720',
+                '3' => 'Monday-Friday:',
+                '4' => 'Saturday:',
+                '5' => 'support@shopgrids.com',
+                '6' => 'Our Mobile App',
+                '7' => 'Download on the',
+                '8' => 'App Store',
+                '9' => 'Download on the',
+                '10' => 'Google Play',
+                'information' => 'Information', [
+                    '1' => 'About Us',
+                    '2' => 'Contact Us',
+                    '3' => 'Downloads',
+                    '4' => 'Site Map',
+                    '5' => 'FAQs Page',
+                ],
+            ],
+            'footer_bottom' => [
+                '1' => 'We Accept:',
+                '2' => 'Designed and Developed by',
+                '3' => 'Kelompok 3',
+                '4' => 'Follow Us On:',
+            ]
+        ]
+    ];
+?>
