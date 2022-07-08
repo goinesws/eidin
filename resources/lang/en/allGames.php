fb43fcfbaa612eb25abe8e054e9d84bf4fd3bf8f
goinesws <64315827+goinesws@users.noreply.github.com>
2022-07-08 14:58:17 +0700

    empty allGames

--- resources/lang/en/allGames.php
+++ resources/lang/en/allGames.php
@@ -1,6 +1,7 @@
 <?php
     return [
         'title' => 'All Games',
+        'empty' => 'Oh no! We don\'t have any game yet in our collection. </br>Let\'s contribute by adding new games!',
         'game' => [
             'rating' => 'Ratings',
         ],
--- resources/lang/id/allGames.php
+++ resources/lang/id/allGames.php
@@ -1,6 +1,7 @@
 <?php
     return [
         'title' => 'Semua Game',
+        'empty' => 'Oh tidak! Belum ada game yang tersedia. </br>Ayo berkontribusi untuk menambahkan game baru!',
         'game' => [
             'rating' => 'Penilaian',
         ],
