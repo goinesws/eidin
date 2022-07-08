1d16cd7c8f58d044ec864e5cddc22a2b3238169d
christopherkesuma <dnlkusuma1@gmail.com>
2022-07-08 10:54:49 +0700

    pending game lang + lang id

---
+++ resources/lang/id/addTagGenre.php
@@ -0,0 +1,9 @@
+<?php
+    return[
+        'back' => 'Kembali',
+        'add_tag' => 'Tambah Tag',
+        'tag_name' => 'Nama Tag',
+        'add_genre' => 'Tambah Kategori',
+        'genre_name' => 'Nama Kategori',
+    ];
+?>
---
+++ resources/lang/id/pendingGame.php
@@ -0,0 +1,8 @@
+<?php
+    return [
+        'title' => 'Game Tertunda',
+        'empty' => 'Sepertinya para developer sedang bekerja keras membuat beberapa game.<br>Tidak ada game yang perlu di review untuk saat ini.',
+        'deny' => 'Tolak',
+        'publish' => 'Terbitkan',
+    ];
+?>
